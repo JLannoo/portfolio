687cf7f13b52f5228638af444d46218526da97cb
JLannoo <julianlannoo@gmail.com>
2022-07-31 00:41:21 -0300

    Migrate to Vite

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import pluginReact from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	// This changes the out put dir from dist to build
+	// comment this out if that isn't relevant for your project
+	build: {
+		outDir: 'build',
+	},
+	server: {
+		host: 'localhost',
+		port: 3000,
+		open: true,
+	},
+	plugins: [
+		pluginReact(),
+		svgrPlugin({
+			svgrOptions: {
+				icon: true,
+				// ...svgr options (https://react-svgr.com/docs/options/)
+			},
+		}),
+	],
+});
