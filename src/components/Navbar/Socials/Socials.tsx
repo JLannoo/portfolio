99c5247c1f5202731236e67226706a5f6b4c3703
JLannoo <julianlannoo@gmail.com>
2023-08-11 13:19:19 -0300

    Add Medium to socials

--- src/components/Navbar/Socials/Socials.tsx
+++ src/components/Navbar/Socials/Socials.tsx
@@ -3,25 +3,32 @@ import css from './Socials.module.scss';
 
 import { ReactComponent as Github } from '../../../assets/github.svg';
 import { ReactComponent as Linkedin } from '../../../assets/linkedin.svg';
+import { ReactComponent as Medium } from '../../../assets/medium.svg';
 
 interface SocialsProps {
 	color?: string;
-	show?: 'github' | 'linkedin' | 'both';
+	show?: 'github' | 'linkedin' | 'medium' | 'all';
 }
 
-export default function Socials({ color = 'white', show = 'both' }: SocialsProps) {
+export default function Socials({ color = 'white', show = 'all' }: SocialsProps) {
 	return (
 		<div className={css.Socials}>
 			{
-				(show === 'both' || show === 'linkedin') &&
-				<a href="https://www.linkedin.com/in/juli%C3%A1n-lann%C3%B3o/" target="_blank" rel="noopener noreferrer" style={{ color }}>
+				(show === 'all' || show === 'linkedin') &&
+				<a title="LinkedIn" href="https://www.linkedin.com/in/juli%C3%A1n-lann%C3%B3o/" target="_blank" rel="noopener noreferrer" style={{ color }}>
 					<Linkedin/>
 				</a>
 			}
 			{
-				(show === 'both' || show === 'github') &&
-				<a href="https://github.com/JLannoo/" target="_blank" rel="noopener noreferrer" style={{ color }}>
+				(show === 'all' || show === 'github') &&
+				<a title="Github" href="https://github.com/JLannoo/" target="_blank" rel="noopener noreferrer" style={{ color }}>
 					<Github/>
+				</a>
+			}
+			{
+				(show === 'all' || show === 'medium') &&
+				<a title="Medium" href="https://medium.com/@julianlannoo" target="_blank" rel="noopener noreferrer" style={{ color }}>
+					<Medium/>
 				</a>
 			}
 		</div>
